2858bae4b1f22d1464acdc2d7ed2d479d2f54730
Michalvp <92635437+Michalvp@users.noreply.github.com>
2026-05-08 15:09:17 +0200

    re-written to cv2

--- assets/Reader.py
+++ assets/Reader.py
@@ -1,13 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
-def generuj_obraz_z_log_odds(nazwa_pliku):
-    surowy_grid = np.load(nazwa_pliku)
-    prob_grid = 1.0 - (1.0 / (1.0 + np.exp(surowy_grid)))
-    obrazek = 1.0 - prob_grid
-    plt.imshow(obrazek, cmap='gray', origin='lower')
-    plt.colorbar(label='Prawdopodobieństwo braku przeszkody')
-    plt.title(f"Wygenerowano z: {nazwa_pliku}")
-    nazwa_png = nazwa_pliku.replace(".npy", ".png")
-    plt.savefig(nazwa_png, dpi=300)
-    plt.show()
-generuj_obraz_z_log_odds("submap2.npy")
+import cv2
+def generuj_obraz_z_log_odds(file_name):
+    raw_grid = np.load(file_name)
+    prob_grid = 1.0 - (1.0 / (1.0 + np.exp(raw_grid)))
+    image = 1.0 - prob_grid
+    imagecv2 = (image*255.0).astype(np.uint8)
+    name = file_name.replace(".npy", "_opencv.png")
+    cv2.imwrite(name, imagecv2)
+    window_name = "map"
+    cv2.imshow(window_name, imagecv2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+generuj_obraz_z_log_odds("submap0.npy")
